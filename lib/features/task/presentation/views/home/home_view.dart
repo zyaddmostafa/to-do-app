9246a878760ec8f7d41c3c13b5c7b199e1bc7600
zyadmostafa <zyadmo026@gmail.com>
2024-11-27 20:54:57 +0200

    create task component

--- lib/features/task/presentation/views/home/home_view.dart
+++ lib/features/task/presentation/views/home/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:date_picker_timeline/date_picker_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_icon_class/font_awesome_icon_class.dart';
 import 'package:intl/intl.dart';
 import 'package:to_do_app/core/utils/app_assets.dart';
 import 'package:to_do_app/core/utils/app_color.dart';
@@ -11,64 +12,140 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-          onPressed: () {},
-          backgroundColor: AppColor.primaryColor,
-          child: const Icon(
-            Icons.add,
-            color: AppColor.secondaryColor,
-          ),
+        child: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+        onPressed: () {},
+        backgroundColor: AppColor.primaryColor,
+        child: const Icon(
+          Icons.add,
+          color: AppColor.secondaryColor,
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                DateFormat.yMMMMd().format(DateTime.now()),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              DateFormat.yMMMMd().format(DateTime.now()),
+              style: Theme.of(context)
+                  .textTheme
+                  .displayLarge!
+                  .copyWith(fontSize: 24),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Text(
+              AppString.today,
+              style: Theme.of(context)
+                  .textTheme
+                  .displayLarge!
+                  .copyWith(fontSize: 24),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            DatePicker(
+              height: 94,
+              DateTime.now(),
+              initialSelectedDate: DateTime.now(),
+              selectionColor: AppColor.primaryColor.withOpacity(0.7),
+              selectedTextColor: Colors.white,
+              dateTextStyle: Theme.of(context).textTheme.displaySmall!,
+              dayTextStyle: Theme.of(context).textTheme.displaySmall!,
+              monthTextStyle: Theme.of(context).textTheme.displaySmall!,
+              onDateChange: (date) {
+                // New date selected
+                // setState(() {
+                //   _selectedValue = date;
+                // });
+              },
+            ),
+            const SizedBox(
+              height: 11,
+            ),
+            taskComponent(context),
+          ],
+        ),
+      ),
+    ));
+  }
+
+  Card taskComponent(BuildContext context) {
+    return Card(
+      color: AppColor.red,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Task 1',
+                  style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                        fontSize: 24,
+                      ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: Row(
+                    children: [
+                      const Icon(
+                        FontAwesomeIcons.clock,
+                        color: AppColor.secondaryColor,
+                        size: 24,
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        "${DateFormat.jm().format(DateTime.now())} - ${DateFormat.jm().format(DateTime.now().add(const Duration(hours: 2)))}",
+                        style: Theme.of(context)
+                            .textTheme
+                            .displayMedium!
+                            .copyWith(fontSize: 16),
+                      ),
+                      const VerticalDivider(
+                        color: AppColor.red,
+                        thickness: 10,
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  'Learn Flutter',
+                  style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                        fontSize: 24,
+                      ),
+                )
+              ],
+            ),
+            const SizedBox(
+              width: 95,
+            ),
+            Container(
+              height: 60,
+              width: 0.6,
+              color: AppColor.secondaryColor,
+            ),
+            RotatedBox(
+              quarterTurns: 3,
+              child: Text(
+                AppString.toda,
                 style: Theme.of(context)
                     .textTheme
-                    .displayLarge!
-                    .copyWith(fontSize: 24),
+                    .displayMedium!
+                    .copyWith(fontSize: 16),
               ),
-              const SizedBox(
-                height: 12,
-              ),
-              Text(
-                AppString.today,
-                style: Theme.of(context)
-                    .textTheme
-                    .displayLarge!
-                    .copyWith(fontSize: 24),
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              DatePicker(
-                height: 94,
-                DateTime.now(),
-                initialSelectedDate: DateTime.now(),
-                selectionColor: AppColor.primaryColor.withOpacity(0.7),
-                selectedTextColor: Colors.white,
-                dateTextStyle: Theme.of(context).textTheme.displaySmall!,
-                dayTextStyle: Theme.of(context).textTheme.displaySmall!,
-                monthTextStyle: Theme.of(context).textTheme.displaySmall!,
-                onDateChange: (date) {
-                  // New date selected
-                  // setState(() {
-                  //   _selectedValue = date;
-                  // });
-                },
-              ),
-              const SizedBox(
-                height: 11,
-              ),
-              const NoTaskWidget()
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
